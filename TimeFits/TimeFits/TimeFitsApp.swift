12da9d53f8fd9f21bde54d03295510cc567eb8e2
DianneK13 <mclarar.almeida@gmail.com>
2026-06-25 09:55:44 -0300

    feat: add initial content from `XCode`

---
+++ TimeFits/TimeFits/TimeFitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeFitsApp.swift
+//  TimeFits
+//
+//  Created by Maria Clara Rodrigues de Almeida on 19/06/26.
+//
+
+import SwiftUI
+
+@main
+struct TimeFitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
