ac5337c20d8737a6a9bad79a5b3221dea29e6cae
DianneK13 <mclarar.almeida@gmail.com>
2026-06-29 19:10:48 -0300

    feat: create a local DB for the exercises in `ExerciciosData`

--- TimeFits/TimeFits/Model/ExerciciosData.swift
+++ TimeFits/TimeFits/Model/ExerciciosData.swift
@@ -5,4 +5,56 @@
 //  Created by Maria Clara Rodrigues de Almeida on 25/06/26.
 //
 
-import Foundation
+struct ExerciciosData {
+    let nome: String
+    let imagem: String
+    let velocidadeDeRepSecs: Int
+    let tempoDeDescansoSecs: Int
+    let quantDeSets: Int
+}
+
+let data: [ExerciciosData] = [
+    // Flexões (Maria Clara)
+    .init(
+        nome: "Flexão de joelho",
+        imagem: "kneePushUp",
+        velocidadeDeRepSecs: 5,
+        tempoDeDescansoSecs: 120,
+        quantDeSets: 3
+    ),
+    .init(
+        nome: "Flexão completa",
+        imagem: "classicPushUP",
+        velocidadeDeRepSecs: 5,
+        tempoDeDescansoSecs: 150,
+        quantDeSets: 3
+    ),
+    .init(
+        nome: "Flexão fechada",
+        imagem: "narrowPushUp",
+        velocidadeDeRepSecs: 5,
+        tempoDeDescansoSecs: 150,
+        quantDeSets: 2
+    ),
+    .init(
+        nome: "Flexão Escalonada Lateral",
+        imagem: "sideStaggeredPushUp",
+        velocidadeDeRepSecs: 5,
+        tempoDeDescansoSecs: 150,
+        quantDeSets: 2
+    ),
+    .init(
+        nome: "Flexão arqueiro",
+        imagem: "archerPushUp",
+        velocidadeDeRepSecs: 5,
+        tempoDeDescansoSecs: 180,
+        quantDeSets: 3
+    ),
+    
+    // ____ (Lais)
+    
+    
+    // ____ (Luisa)
+    
+]
+
